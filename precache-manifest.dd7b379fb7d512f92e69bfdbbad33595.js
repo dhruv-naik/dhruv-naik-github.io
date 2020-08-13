eb18cfc3d6ddf4908dbab8345f0eca0685b252cf
dhruvnaik111 <68032436+dhruvnaik111@users.noreply.github.com>
2020-08-13 10:48:39 -0700

    Updates

---
+++ precache-manifest.dd7b379fb7d512f92e69bfdbbad33595.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1dcfe47dc1f475011f147ec7a22e563",
+    "url": "/koaanstudiopage.github.io./index.html"
+  },
+  {
+    "revision": "d20878f339515548c9ad",
+    "url": "/koaanstudiopage.github.io./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b134a9d19d40ea9ca50d",
+    "url": "/koaanstudiopage.github.io./static/js/2.1c802d8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/koaanstudiopage.github.io./static/js/2.1c802d8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d20878f339515548c9ad",
+    "url": "/koaanstudiopage.github.io./static/js/main.322e66eb.chunk.js"
+  },
+  {
+    "revision": "7dca8f43424b406c758e",
+    "url": "/koaanstudiopage.github.io./static/js/runtime-main.a27edb82.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/koaanstudiopage.github.io./static/media/logo.5d5d9eef.svg"
+  }
+]);
